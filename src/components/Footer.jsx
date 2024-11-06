7e1a021e975174fb591ff605cd397958367aeea8
Afrin kona <152356092+afrinsultanakona00907@users.noreply.github.com>
2024-11-06 21:46:16 +0600

    3rd commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ const Footer = () => {
 		<footer className="py-16 bg-white text-center">
 			<h2 className="font-bold text-2xl mb-2">Gadget Heaven</h2>
 			<p className="text-gray-600">
-				Leading the way in cutting-edge technology and innovation.
+				Leading the way in cutting  edge technology and innovation.
 			</p>
 			<hr className="max-w-5xl mx-auto my-6" />
 			<section className="max-w-2xl w-11/12 mx-auto flex flex-col md:flex-row justify-between gap-4">
@@ -11,7 +11,7 @@ const Footer = () => {
 					<h4 className="text-[#1F2937] font-bold">Services</h4>
 					<a href="/">Product Support</a>
 					<a href="/">Order Tracking</a>
-					<a href="/">Shipping & Delivery</a>
+					<a href="/">Shipping and Delivery</a>
 					<a href="/">Returns</a>
 				</nav>
 				<nav className="flex flex-col text-gray-500">
